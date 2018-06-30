d9e31a0c9d54b94a986f9633f0b81e9ae83e9272
Kolja Maier <koljacorneliusmaier@gmail.com>
2018-06-30 02:35:10 +0200

    created new remote tracking branch

--- recfun/src/main/scala/recfun/Bane.scala
+++ recfun/src/main/scala/recfun/Bane.scala
@@ -1,5 +1,10 @@
 package recfun
 
+import com.fasterxml.jackson.databind.JsonNode
+import scala.collection.JavaConversions._
+
+import scala.util.parsing.json.JSON
+
 object Bane {
 
     def main(args: Array[String]) {
@@ -11,10 +16,34 @@ object Bane {
           |    "clients":"string",
           |    "parameter":"string",
           |    "channel":"string",
-          |    "metadata":{
-          |        "meta1":"string",
+          |    "metadata":[{
+          |        "meta1":"stri1ng",
           |        "meta2":"string",
-          |        "meta3":"string"
+          |        "meta3":"string",
+          |        "lol" :[{
+          |              "zack": "bumm"
+          |        },
+          |        {
+          |         "zack": "bumm232"
+          |        }
+          |        ]
+          |    },
+          |    {
+          |        "meta1":"dasd",
+          |        "meta2":"ddd",
+          |        "meta3":"stridng",
+          |        "lol" :[{
+          |              "zack": "bumm"
+          |        },
+          |        {
+          |         "zack": "bumm232"
+          |        }
+          |        ]
+          |    }
+          |    ],
+          |    "tester": {
+          |      "test1": "dda",
+          |      "tee": {"tetl": "b", "waku": "j"}
           |    }
           |},
           |"timestamp":"string"}
@@ -31,18 +60,251 @@ object Bane {
     {"some": "json data"}
 """
 
+      ////////////// PARSING WITH JACKSON
       val mapper = new ObjectMapper
       mapper.registerModule(DefaultScalaModule)
 
       val test = mapper.readValue(jsonString, classOf[Map[String, String]])
-      test.foreach(println)
+      //test.foreach(x => println(x + "neuer print"))
+      //println(test) // Map(body -> Map(method -> string, parameter -> string, channel -> string, clients -> string, metadata -> Map(meta1 -> string, meta2 -> string, meta3 -> string), events -> string), timestamp -> string)
+      //println(test("body"))
       val t = test.get("test")
-      println(t)
+      //println("hier kommt test.get('test')")
+      //println(t) // None
       val parsedJson = mapper.readValue(jsonString,classOf[Map[String, Object]])
-      println(parsedJson)
-      val p = parsedJson("body")
+      //println(parsedJson)
+
+
+
+      val rootNode: JsonNode = mapper.readTree(jsonString)
+      println("sizeeee  " + rootNode.size) // 2 -> "body" und "timestamp"
+      println("size " + rootNode.get("body").size) // 7
+
+
+      // Da ist das Ding :_)
+      val b = for(i <- rootNode.findValue("tester").fields()) yield {
+        i.getKey -> mapper.writeValueAsString(i.getValue)
+      }
+
+
+      val bMap = b.toMap
+
+      println(bMap("test1") + "   " + mapper.readValue(bMap("tee"), classOf[Map[String, String]]))
+
+      println(bMap("tee"))
+
+
+      def parseJsonToListMap(root: JsonNode, key: String): List[Map[String,String]] = {
+        val jNode = root.findValue(key)
+        if(jNode.isArray) parseJsonArrayToListMap(jNode.elements.toList)
+        else List(extractMap(jNode))
+      }
+
+      def extractMap(root: JsonNode): Map[String,String] = {
+       val l = for(i <- root.fields()) yield {
+          i.getKey -> mapper.writeValueAsString(i.getValue)
+        }
+        l.toMap
+      }
+
+      def parseJsonArrayToListMap(jsonArray: List[JsonNode]): List[Map[String,String]] = {
+        println("Hier angekommen")
+        jsonArray match {
+          case x :: xs => extractMap(x) :: parseJsonArrayToListMap(xs)
+          case List() => List()
+          //case List() => List(extractMap(jsonArray))
+        }
+      }
+
+
+      println("Test Funcs")
+      val parsed = parseJsonToListMap(rootNode, "tester")
+      println(bMap("tee"))
+      println(parsed.head("tee"))
+
+      val parsed1 = parseJsonToListMap(rootNode, "metadata")
+      println(parsed1.head("meta1"))
+      println(parsed1.tail.head("meta1"))
+
+      println("End Test")
+
+
+
+      val phoneNumbersNode1 = rootNode.findValues("lol") // List[JsonNode]
+      //println(phoneNumbersNode1 + "test123")
+
+
+      val personalInformationNode = rootNode.get("body")
+      val personalInformationMap : Map[String,String] = mapper.convertValue(personalInformationNode, classOf[Map[String,String]])
+      println("test"+ personalInformationMap)
+      println("testtiii"+personalInformationMap("metadata"))
+      println(personalInformationMap.get("parameter"))
+      println("hört hört" + personalInformationMap.get("metadata"))
+
+      val personalInformationNode2 = rootNode.elements()
+
+      println("gier"+rootNode.elements)
+      for(i <- personalInformationNode2) {
+        println(i)
+      }
+
+      println("for fieldNames")
+      for(i <- rootNode.fieldNames()) {
+        println("tata" + i)
+      }
+
+
+      println("for fields")
+      val zz1 = for(i <- rootNode.fields()) {
+        if(i.getKey=="body") println(mapper.writeValueAsString(i.getValue))
+        if(i.getKey=="body") println(i.getValue.isArray)
+      }
+
+
+
+
+
+      //zz1.foreach(println)
+
+      println("ok")
+      val phoneNumbersNode3 : JsonNode = rootNode.findValue("metadata")
+
+      val z = phoneNumbersNode3.elements.toList
+      val y = z match {
+        case x :: y => println(mapper.writeValueAsString(x))
+      }
+
+
+      println(z)
+      //phoneNumbersNode3.fields.foreach(println)
+      val tt: Map[String,String] = mapper.convertValue(phoneNumbersNode3.elements.next, classOf[Map[String,String]])
+      //println(phoneNumbersNode3.elements().next().mapper.asInstanceOf[Map[String,String]])
+      println(tt)
+      println(tt("lol"))
+      println("ok")
+
+      rootNode.fieldNames().foreach(println(_))
+
+
+
+
+      personalInformationMap.foreach { a: (String, String) =>
+        //println(s"$a")
+      }
+
+
+      println("done")
+
+      //println(phoneNumbersNode1.get(0).findValuesAsText("lol"))
+      val phoneNumbersNode : JsonNode = rootNode.findValue("lol")
+      println(phoneNumbersNode)
+
+
+      val notexist = rootNode.path("bla")
+      if(notexist.isMissingNode) println("Missing Node")
+
+      val p: Object = parsedJson("body")
       println(parsedJson("body"))
 
+
+      /*
+      //Für ein JSON Array
+      public Iterator<JsonNode> readPhoneNumbers(){
+     JsonNode phoneNumbersNode = rootNode.path("phoneNumbers");
+     Iterator<JsonNode> elements = phoneNumbersNode.elements();
+     while(elements.hasNext()){
+         JsonNode phoneNode = elements.next();
+         logger.info("\n----------------------------\nPhone Numbers = "+phoneNode.asLong());
+     }
+     return elements;
+ }
+       */
+
+      test match {
+        case e: Map[String, Any] => {
+          //println(e) //output: Map(name -> OZKA, monthRevenue -> 1000.75, developer -> true, birthDate -> 1981-02-08T20:00:00.000Z, id -> 1.0)
+          //e.foreach { pair =>
+          //  println(pair._1 + ":" + pair._2)
+          //}
+          //e.foreach { case (key: String, value: String) => println(value) }
+        }
+        case _ => println("Failed.")
+      }
+
+      //////////// PARSING WITH SCALA JSON UTIL
+
+
+
+
+      //
+
+      println("testAnfang")
+
+      val jsonMap = JSON.parseFull(jsonString).getOrElse(0).asInstanceOf[Map[String,String]]
+      val innerMap = jsonMap("body").asInstanceOf[Map[String,String]]
+      innerMap.keys //will give keys
+      innerMap("metadata") //will give value for any key anykey
+
+
+
+      val result = JSON.parseFull(jsonString)
+      result match {
+        // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
+        case Some(map: Map[String, Any]) => println(map)
+        case None => println("Parsing failed")
+        case other => println("Unknown data structure: " + other)
+      }
+
+
+
+
+
+
+
+
+      println("testEnde")
+      //
+
+      case class Tweet(user: String, text: String, retweets: Int)
+
+      import scala.util.parsing.json._
+
+      def getList[T](s: String): List[T] =
+        JSON.parseFull(s).get.asInstanceOf[List[T]]
+
+      def getMap(s: String): Map[String, Any] =
+        JSON.parseFull(s).get.asInstanceOf[Map[String, Any]]
+
+
+      /*def getMppss(json: String): List[Map[String,String]] =
+        for (map <- getList[Map[String, String]](json)) yield {
+          map
+        }*/
+
+      //val zZ = getList[Map[String, String]](jsonString)
+      val pp = JSON.parseFull(jsonString).get.asInstanceOf[Map[String,Any]]
+      //val pp1 = JSON.parseFull(jsonString).get.asInstanceOf[List[Map[String,Any]]]
+
+      println(pp("body"))
+      //println("bllaslslalsalsa")
+
+      def getTweetData(user: String, json: String): List[Tweet] = {
+        // is list
+        val l = getList[Map[String, Any]](json)
+        for (map <- l) yield {
+          val text = map("text")
+          val retweets = map("retweets")
+          new Tweet(user, text.toString, retweets.toString.toDouble.toInt)
+        }
+      }
+
+
+
+      val tweets = JSON.parseFull(jsonString).get.asInstanceOf[Map[String,Any]]
+      //println("HIER")
+      //println(tweets("body").asInstanceOf[Map[String,Any]]("method"))
+      //val tweeL = getList[Map[String, Any]](jsonString)
+      //println(tweeL)
 
       /*jsonString.flatMap(record => {
         try {
@@ -65,12 +327,12 @@ object Bane {
 
       //////////////
       val m1 = Map("fname" -> "Al", "lname" -> "Alexander")
-      for ((k,v) <- m1) printf("key: %s, value: %s\n", k, v)
+      //for ((k,v) <- m1) printf("key: %s, value: %s\n", k, v)
 
      // for ((k,v) <- test) printf("key: %s, value: %s\n", k, v)
 
-      m1 foreach (x => println (x._1 + "-->" + x._2))
-      m1 foreach {case (key, value) => println (key + "-->" + value)}
+      //m1 foreach (x => println (x._1 + "-->" + x._2))
+      //m1 foreach {case (key, value) => println (key + "-->" + value)}
 
 
       val p1Ratings = Map(
@@ -103,5 +365,72 @@ object Bane {
 
 
     }
+
+  val request =
+    """
+      |"{"id":1,"name":"OZKA","birthDate":"1981-02-08T20:00:00.000Z","monthRevenue":1000.75,"developer":true}"
+    """.stripMargin
+  //println(request)//{"id":1,"name":"OZKA","birthDate":"1981-02-08T20:00:00.000Z","monthRevenue":1000.75,"developer":true}
+  val result1: Option[Any] = scala.util.parsing.json.JSON.parseFull(request)
+  //klappt nicht!
+  //val result: Map[String, Any] = JSON.parseFull(request).get.asInstanceOf[Map[String,Any]]
+  //println(result1)
+
+
+  result1 match {
+    case Some(e: Any) => {
+     // println("das ist die Map")
+      //println("das ist die map"+ e) //output: Map(name -> OZKA, monthRevenue -> 1000.75, developer -> true, birthDate -> 1981-02-08T20:00:00.000Z, id -> 1.0)
+      //e.foreach { pair =>
+      //  println(pair._1 + ":" + pair._2)
+      //}
+      //e.foreach { case (key: String, value: Any) => println(value) }
+      //println(e)
+    }
+    case None => println("Failed.")
+  }
+
+
+
+
+  //println("ok")
+
+
+  class CC[T] { def unapply(a:Any):Option[T] = Some(a.asInstanceOf[T]) }
+
+  object M extends CC[Map[String, Any]]
+  object L extends CC[List[Any]]
+  object S extends CC[String]
+  object D extends CC[Double]
+  object B extends CC[Boolean]
+
+  val jsonString =
+    """
+      {
+        "languages": [{
+            "name": "English",
+            "is_active": true,
+            "completeness": 2.5
+        }, {
+            "name": "Latin",
+            "is_active": false,
+            "completeness": 0.9
+        }]
+      }
+    """.stripMargin
+
+  val res = for {
+    Some(M(map)) <- List(JSON.parseFull(jsonString))
+    L(languages) = map("languages")
+    M(language) <- languages
+    S(name) = language("name")
+    B(active) = language("is_active")
+    D(completeness) = language("completeness")
+  } yield {
+    (name, active, completeness)
+  }
+
+  assert( res == List(("English",true,2.5), ("Latin",false,0.9)))
+
 
 }
