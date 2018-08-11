4dd8a66c6a4b89ffea907d83b54db43448ff8e07
Kolja Maier <koljacorneliusmaier@gmail.com>
2018-08-11 20:52:11 +0200

    finished tasks 10/10

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -9,13 +9,37 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
+  /*
+  This function takes as input a map from variable name to expressions of their values.
+  Since the expression is derived from the text entered by the user, it is a Signal.
+  The Expr abstract data type is defined as follows
+
+  The Ref(name) case class represents a reference to another variable in the map namedExpressions.
+  The other kinds of expressions have the obvious meaning
+
+  The function should return another map from the same set of variable names to their actual values,
+  computed from their expressions.
+   */
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    val t = for {
+      (k, v) <- namedExpressions
+    } yield { (k -> Signal(eval(v(), namedExpressions))) }
+    t
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => {
+        val ref = getReferenceExpr(name, references)
+        eval(ref, references - name)
+      }
+      case Plus(a, b)   => eval(a, references) + eval(b, references)
+      case Minus(a, b)  => eval(a, references) - eval(b, references)
+      case Times(a, b)  => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    }
   }
 
   /** Get the Expr for a referenced variables.
--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,18 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal(Math.pow(b(),2) - 4*a()*c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal {
+      if(delta() < 0) Set()
+      else {
+        val s1 = ((-1*b()) + (Math.sqrt(delta()))) / (2*a())
+        val s2 = ((-1*b()) - (Math.sqrt(delta()))) / (2*a())
+        Set(s1,s2)
+      }
+    }
   }
 }
